469cd1a6ce7f364a8e07dd14fc91ba82222d75b8
jam-dev <jamalvga2002@gmail.com>
2023-11-18 17:36:18 +0530

    Update metadata in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,10 +31,10 @@ const chains = [
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
 
 const metadata = {
-	name: "Next Starter Template",
-	description: "A Next.js starter template with Web3Modal v3 + Wagmi",
-	url: "https://web3modal.com",
-	icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  name: "Prophets",
+  description: "A Next.js starter template with Web3Modal v3 + Wagmi",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
